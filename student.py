62cb31459a606183b8bd546051d39557405f076e
David Silva <david@davidsilva.pt>
2018-12-09 00:51:00 +0000

    student.py: Atualizada estrutura

--- student.py
+++ student.py
@@ -1,13 +1,15 @@
+import datetime
+
 class Student:
 
     def __init__(self, data):
-        self.id = data[0]
-        self.name = data[1]
-        self.email = data[2]
-        self.nmec = data[3]
-        self.ano = data[4]
-        self.token = data[5]
-        self.userName = data[6]
+        self.id = data[0] or 0
+        self.name = data[1] or ''
+        self.email = data[2] or ''
+        self.nmec = data[3] or 0
+        self.ano = data[4] or datetime.datetime.now().year
+        self.token = data[5] or ''
+        self.username = data[6] or ''
 
     def __str__(self):
-        return self.userName + " " +  self.name + " " + self.email+ " " + self.token+ " " + str(self.nmec)+ " " + str(self.ano)
+        return self.username + " " +  self.name + " " + self.email+ " " + self.token+ " " + str(self.nmec)+ " " + str(self.ano)
